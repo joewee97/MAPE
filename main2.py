ba82df7e85435a9a5a660b10e55659f8c22d16a9
joewee97 <76058234+joewee97@users.noreply.github.com>
2020-12-16 00:47:57 +0800

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+import MySQLdb
+from multiprocessing import Process
+
+from pl import rc_time
+
+#GPIO20, pin 38 for the photoresistor
+pin_to_circuit = 20
+
+def main():
+    while True:
+        p1 = Process(
+            rc_time(pin_to_circuit)
+            #time.sleep() runs through within pla1.py itself
+            #30 minute idle within file pla.py
+        )
+        #multiprocessing engages
+        p1.start()
+
+if __name__ == "__main__":
+    main()
